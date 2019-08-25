9a987848820802ab6fdb7a9940c7cca618cc7aad
cyliong <liongtheprogrammer@gmail.com>
2019-08-25 21:56:19 +0800

    Refactor driver creation to the DriverFactory class

---
+++ src/test/kotlin/com/example/ltp/list/DriverFactory.kt
@@ -0,0 +1,41 @@
+package com.example.ltp.list
+
+import io.appium.java_client.AppiumDriver
+import io.appium.java_client.MobileElement
+import io.appium.java_client.android.AndroidDriver
+import io.appium.java_client.ios.IOSDriver
+import io.appium.java_client.remote.MobileCapabilityType
+import io.appium.java_client.remote.MobilePlatform
+import org.openqa.selenium.remote.DesiredCapabilities
+import java.net.URL
+
+class DriverFactory {
+
+    companion object {
+        private val serviceUrl = URL("http://localhost:4723/wd/hub")
+
+        fun createDriver(): AppiumDriver<MobileElement> {
+            if (System.getProperty("platform") == "ios") {
+                val capabilities = DesiredCapabilities().apply {
+                    setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.IOS)
+                    setCapability(MobileCapabilityType.PLATFORM_VERSION, "12.4")
+                    setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Xs")
+                    setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest")
+                    setCapability(MobileCapabilityType.APP, "/path/to/List.app")
+                }
+                return IOSDriver(serviceUrl, capabilities)
+            } else {
+                val capabilities = DesiredCapabilities().apply {
+                    setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID)
+                    setCapability(MobileCapabilityType.PLATFORM_VERSION, "9")
+                    setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator")
+                    setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2")
+                    setCapability(MobileCapabilityType.APP, "/path/to/list.apk")
+                }
+                return AndroidDriver(serviceUrl, capabilities)
+            }
+        }
+
+    }
+
+}
--- src/test/kotlin/com/example/ltp/list/step/Steps.kt
+++ src/test/kotlin/com/example/ltp/list/step/Steps.kt
@@ -1,15 +1,10 @@
 package com.example.ltp.list.step
 
+import com.example.ltp.list.DriverFactory
 import io.appium.java_client.AppiumDriver
 import io.appium.java_client.MobileElement
-import io.appium.java_client.android.AndroidDriver
-import io.appium.java_client.ios.IOSDriver
-import io.appium.java_client.remote.MobileCapabilityType
-import io.appium.java_client.remote.MobilePlatform
 import io.cucumber.core.api.Scenario
 import io.cucumber.java8.En
-import org.openqa.selenium.remote.DesiredCapabilities
-import java.net.URL
 import java.util.concurrent.TimeUnit
 
 open class Steps : En {
@@ -18,24 +13,7 @@ open class Steps : En {
 
     init {
         Before { _: Scenario ->
-            val serviceUrl = URL("http://localhost:4723/wd/hub")
-            DesiredCapabilities().apply {
-                driver = if (System.getProperty("platform") == "ios") {
-                    setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.IOS)
-                    setCapability(MobileCapabilityType.PLATFORM_VERSION, "12.4")
-                    setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Xs")
-                    setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest")
-                    setCapability(MobileCapabilityType.APP, "/path/to/List.app")
-                    IOSDriver(serviceUrl, this)
-                } else {
-                    setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID)
-                    setCapability(MobileCapabilityType.PLATFORM_VERSION, "9")
-                    setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator")
-                    setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2")
-                    setCapability(MobileCapabilityType.APP, "/path/to/list.apk")
-                    AndroidDriver(serviceUrl, this)
-                }
-            }
+            driver = DriverFactory.createDriver()
             driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS)
         }
 
