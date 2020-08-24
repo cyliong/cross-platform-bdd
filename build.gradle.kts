24e16bb945f31c5fba5e70fd60718ebe5a69158c
cyliong <liongtheprogrammer@gmail.com>
2020-08-24 11:39:52 +0800

    Migrate Gradle build script from Groovy DSL to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.4.0"
+}
+
+val cucumberVersion = "6.5.1"
+val appiumVersion = "7.3.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("io.cucumber:cucumber-java8:$cucumberVersion")
+    testImplementation("io.cucumber:cucumber-junit:$cucumberVersion")
+    testImplementation("io.cucumber:cucumber-picocontainer:$cucumberVersion")
+    testImplementation("io.appium:java-client:$appiumVersion")
+}
+
+val cucumberRuntime: Configuration by configurations.creating {
+    extendsFrom(configurations.testImplementation.get())
+}
+
+arrayOf("Android", "IOS").forEach { platform ->
+    tasks.register<JavaExec>("test$platform") {
+        main = "io.cucumber.core.cli.Main"
+        classpath = cucumberRuntime + sourceSets.main.get().output + sourceSets.test.get().output
+        args = listOf("--plugin", "pretty", "--glue", "com.example.ltp.list", "src/test/resources")
+        systemProperty("platform", platform.toLowerCase())
+    }
+}
