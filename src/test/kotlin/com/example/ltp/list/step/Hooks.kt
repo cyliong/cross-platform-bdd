be19d55ad04ace1614ba55358ae3a35f402dd447
cyliong <liongtheprogrammer@gmail.com>
2020-11-05 17:01:52 +0800

    Take screenshot if test is failed

--- src/test/kotlin/com/example/ltp/list/step/Hooks.kt
+++ src/test/kotlin/com/example/ltp/list/step/Hooks.kt
@@ -3,11 +3,18 @@ package com.example.ltp.list.step
 import com.example.ltp.list.Context
 import io.cucumber.java8.En
 import io.cucumber.java8.Scenario
+import org.openqa.selenium.OutputType
+import org.openqa.selenium.TakesScreenshot
+import java.io.File
 
 class Hooks(context: Context) : En {
 
     init {
-        After { _: Scenario ->
+        After { scenario: Scenario ->
+            if (scenario.isFailed) {
+                val file = (context.driver as TakesScreenshot).getScreenshotAs(OutputType.FILE)
+                file.copyTo(File("screenshot.png"), true)
+            }
             context.driver.quit()
         }
     }
