8aa4fe95bf7f312218888de88e4c99c393347360
cyliong <liongtheprogrammer@gmail.com>
2020-11-18 21:26:59 +0800

    Update platform version for iOS driver

--- src/test/kotlin/com/example/ltp/list/DriverFactory.kt
+++ src/test/kotlin/com/example/ltp/list/DriverFactory.kt
@@ -18,7 +18,7 @@ class DriverFactory {
             if (System.getProperty("platform") == "ios") {
                 val capabilities = DesiredCapabilities().apply {
                     setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.IOS)
-                    setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.1")
+                    setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.2")
                     setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 12 Pro")
                     setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest")
                     setCapability(MobileCapabilityType.APP, "/path/to/List.app")
